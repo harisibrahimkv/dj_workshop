8d9bf9e537d3151ee8835e0d62018e6981d5768a
Haris Ibrahim K. V <blucalvin@gmail.com>
2014-10-08 11:54:59 +0530

    Passes the date into the template and renders it there

--- visualizer/visualize/views.py
+++ visualizer/visualize/views.py
@@ -7,4 +7,5 @@ import datetime
 
 def entity_visualizer(request):
     current_date = datetime.datetime.now()
-    return render(request, "hello_world.html")
+    var_dictionary = {"date_now": current_date}
+    return render(request, "hello_world.html", var_dictionary)
