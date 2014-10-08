496c8a40b3b98878203519695d75a4c0d592cb03
Haris Ibrahim K. V <blucalvin@gmail.com>
2014-10-08 11:29:47 +0530

    Gets the current date

--- visualizer/visualize/views.py
+++ visualizer/visualize/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+import datetime
+
 # Create your views here.
 
 def entity_visualizer(request):
+    current_date = datetime.datetime.now()
     return render(request, "hello_world.html")
