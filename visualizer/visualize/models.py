9c5ed00f7294e3184f40462ea1d6e1532d265350
Haris Ibrahim K. V <blucalvin@gmail.com>
2014-10-11 10:44:39 +0530

    Redefines the Tweet modes in the twitter app

--- visualizer/visualize/models.py
+++ visualizer/visualize/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Tweet(models.Model):
-    hashtag = models.CharField(max_length=85)
-    author = models.CharField(max_length=85)
-    created_at = models.DateTimeField()
-    text = models.CharField(max_length=225)
