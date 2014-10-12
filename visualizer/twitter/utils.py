04a90aa4c9704c6952a6de151184b8b9d6a5e933
Haris Ibrahim K. V <blucalvin@gmail.com>
2014-10-12 12:48:07 +0530

    Fits in Redis to count and store popular tweets

--- visualizer/twitter/utils.py
+++ visualizer/twitter/utils.py
@@ -1,4 +1,5 @@
 import json
+import redis
 import datetime
 # from twython import Twython
 
@@ -19,16 +20,32 @@ def fetch_tweets(hashtag):
     tweets = json.loads(f.read())
     return tweets
 
-def save_tweets(tweets, hashtag):
+def archive_tweets(tweets, hashtag):
+    rq = redis.StrictRedis()
+    rq.delete("tweets_count", "popular_tweets")
     for tweet in tweets:
-        author = tweet['user']['screen_name']
-        created_at = datetime.datetime.strptime(
-            tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'
+        save_tweet(tweet, hashtag)
+        rq.incr("tweets_count", amount = 1)
+
+def save_tweet(tweet, hashtag):
+    author = tweet['user']['screen_name']
+    created_at = datetime.datetime.strptime(
+        tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y'
         )
-        text = tweet['text']
-        tweet, created = Tweet.objects.get_or_create(
-            hashtag = hashtag,
-            author = author,
-            created_at = created_at,
-            text = text
-        )
+    text = tweet['text']
+    t, created = Tweet.objects.get_or_create(
+        hashtag = hashtag,
+        author = author,
+        created_at = created_at,
+        text = text
+    )
+    if created:
+        if 'retweeted_status' in tweet:
+            tw = save_tweet(tweet['retweeted_status'], hashtag)
+            score_entity(tw.id, tweet['retweeted_status']['retweet_count'])
+
+    return t
+
+def score_entity(id, rt_count):
+    rq = redis.StrictRedis(host='localhost', port=6379, db=0)
+    rq.zadd("popular_tweets", rt_count, id)
